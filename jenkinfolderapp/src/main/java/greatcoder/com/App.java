50fb0a9342fbbe6fe1b73ac119082e190c4e3ada
root <root@ip-172-31-5-209.ec2.internal>
2022-11-27 13:45:03 +0000

    hi

---
+++ jenkinfolderapp/src/main/java/greatcoder/com/App.java
@@ -0,0 +1,13 @@
+package greatcoder.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
